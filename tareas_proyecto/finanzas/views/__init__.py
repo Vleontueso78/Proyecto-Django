f65ba6aea9b6ddb797be8cc88b22e4ce85b95454
Vleontueso78 <vleontueso78@gmail.com>
2025-11-03 23:35:09 -0300

    Elimine la carpeta wiews.py de finanzas

--- tareas_proyecto/finanzas/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dashboard_view import FinanzasDashboardView
-from .registro_views import RegistroListView, RegistroCreateView
-from .objetivo_views import ObjetivoListView, ObjetivoCreateView
-from .utils_views import *
